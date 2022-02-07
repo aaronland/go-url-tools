7667c4972801c0e336fe5a11932fa30cd53daa61
thisisaaronland <aaron@localhost>
2022-02-07 14:44:58 -0800

    update urlencode to read from STDIN

---
+++ Makefile
@@ -0,0 +1,2 @@
+cli:
+	go build -mod vendor -o bin/urlencode cmd/urlencode/main.go
--- cmd/urlencode/main.go
+++ cmd/urlencode/main.go
@@ -1,17 +1,46 @@
-// urlencode URL-encodes one or more command-line arguments emitting each to STDOUT.
+// urlencode URL-encodes one or more command-line arguments emitting each to STDOUT. For example:
+//
+//	$> ./bin/urlencode 'csv://?archive=archive.tar.gz'
+//	csv%3A%2F%2F%3Farchive%3Darchive.tar.gz
+// Or:
+//	$> echo 'csv://?archive=archive.tar.gz' | bin/urlencode -stdin
+//	csv%3A%2F%2F%3Farchive%3Darchive.tar.gz
 package main
 
 import (
+	"bufio"
 	"flag"
-	"net/url"
 	"fmt"
+	"log"
+	"net/url"
+	"os"
 )
 
 func main() {
 
+	stdin := flag.Bool("stdin", false, "Read input from STDIN")
+
 	flag.Parse()
 
-	for _, str := range flag.Args() {
+	raw := flag.Args()
+
+	if *stdin {
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+		for scanner.Scan() {
+			raw = append(raw, scanner.Text())
+		}
+
+		err := scanner.Err()
+
+		if err != nil {
+			log.Fatalf("Failed to read from STDIN, %v", err)
+		}
+
+	}
+
+	for _, str := range raw {
 		enc := url.QueryEscape(str)
 		fmt.Println(enc)
 	}
